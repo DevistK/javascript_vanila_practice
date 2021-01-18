f9a753899472dba4fd5e053bd3ad6e3ff92ed804
dhs-galaxysoft <dhs@galaxysoft.io>
2021-01-18 10:29:16 +0900

    add : 로직 완성

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,51 @@
+// 대문자로 변수를 지을때 조건은 변하지 않으면서 , 기억하기 어려운 하드코딩된 요소들을 아래와 같이 짓는다.
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]',
   DETAIL_TITLE_SELECTOR = '[data-image-role="title"]',
   THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageUrl, titleText) {
+  "use strict";
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute("src", imageUrl);
+
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+  "use strict";
+  return thumbnail.getAttribute("data-image-url");
+}
+
+function titleFromThumb(thumbnail) {
+  "use strict";
+  return thumbnail.getAttribute("data-image-title");
+}
+
+function setDetailsFromThumb(thumbnail) {
+  "use strict";
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+  "use strict";
+  thumb.addEventListener("click", function (event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+  });
+}
+
+function getThumbnailsArray() {
+  "use strict";
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR),
+    thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+var thumbnails = getThumbnailsArray();
+function initializeEvents() {
+  "use strict";
+  thumbnails.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
