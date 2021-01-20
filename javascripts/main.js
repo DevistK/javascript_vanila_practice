734b1fbb6e2dd240f317fc104434e13fc1c0c7c1
dhs-galaxysoft <dhs@galaxysoft.io>
2021-01-20 09:06:02 +0900

    add : 애니메이션 자바스크립트 로직  추가

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,10 @@
 // 대문자로 변수를 지을때 조건은 변하지 않으면서 , 기억하기 어려운 하드코딩된 요소들을 아래와 같이 짓는다.
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]',
   DETAIL_TITLE_SELECTOR = '[data-image-role="title"]',
+  DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]',
   THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]',
   HIDDEN_DETAIL_CLASS = "hidden-detail",
+  TINY_EFFECT_CLASS = "is-tiny",
   ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
@@ -54,7 +56,12 @@ function hideDetails() {
 
 function showDetails() {
   "use strict";
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  },50);
 }
 
 function addKeyPressHandler() {
