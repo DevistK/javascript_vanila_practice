7732c2ca1bcf61086a5693e24fcf1021fa72932a
donghyeok <tjehdgur1500@naver.com>
2021-06-13 20:10:54 +0900

    add : react used props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// 동적으로 props 엘리먼트를 추가하기 위한 음식 API
+const foodILike = [
+  {
+    id: 1,
+    name: "pizza",
+    src: "https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png",
+  },
+  {
+    id: 2,
+    name: "hambugger",
+    src: "https://img-global.cpcdn.com/recipes/6f1b637530078f17/680x482cq70/bugger-recipe-recipe-main-photo.webp",
+  },
+  {
+    id: 3,
+    name: "ramen",
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Shoyu_Ramen.jpg/250px-Shoyu_Ramen.jpg",
+  },
+];
+
+function Food({ name, picture }) {
+  // props로 해당 컴포넌트의 prop Value를 가져옴
+  // jsx + props 로 컴포넌트 재사용성이 매우용이
+  // props의 prop으로 가져오든 , 해당 prop name 으로 가져오든 컴포넌트의 인자로 전달되기만 하면 정보를 쉽게 가져온다.
+  // html상에서 text를 렌더링하고자 할때 JSX는 그것을 props로 정보를 보내는것과 같음, prop은 string 뿐아니라 object 들도 상관없이 보낼 수 있다.
+  // express ejs , pug 보는 느낌
+  return (
+    <h2>
+      I like {name}
+      <span>
+        <img src={picture} alt={name}></img>
+      </span>
+    </h2>
+  );
+}
+
+// 함수내부에서 컴포넌트를 리턴하게 되면 Application 안에서 호출된 renderFood 는 각각 아이템들을 컴포넌트를 통해 렌더링 하게 된다.
+// 하지만 너무 많은 함수를 만들게 될 경우 보기 힘들어 지는것도 마찬가지
+// function renderFood(dish) {
+//   return <Food name={dish.name} picture={dish.src} />;
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 괄호를 이용해 자바스크립트 사용함을 알리고 그안에서는 자바스크립트 사용이 가능하다 */}
+      {/* 이 경우는 map을 통해 각 객체마다 가지고 있는 name , src 를 Food 컴포넌트의 props 에 전달한다. */}
+      {/* arg 로 전달하는 props는 =propsName을 인자로 전달하면 객체였던 props가 실제 html로 렌더링 한다.*/}
+      {/* map 을 사용하되 직접적으로 컴포넌트를 넣어 prop을 전달하지 않고 map의 인자로 function을 전달하며 분리시킨다. */}
+
+      {/* 기본적으로 컴포넌트의 엘리먼트에는 유니크한 key를 가져야한다. */}
+      {/* 유니크 key를 갖지 않으면 에러를 발생 시키기 때문에 고유하게 만들기 위해서 사용되는 데이터에 id 를 주고 , 컴포넌트 prop에 key name을 준다.*/}
+      {/* 이는 컴포넌트의 인자로 전달되지 않지만 리액트 내부에서 사용하기 위한것 */}
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.src} />
+      ))}
+      {/* {foodILike.map(renderFood)} */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 리액트는 virtual DOM 을 사용해 실제 엘리먼트를 만들어내고
+// 브라우저가 파싱한 html에 엘리먼트들을 추가해준다.
+
+// 리액트는 렌더링 시 하나의 컴포넌트만 렌더링 할 수 있어 컴포넌트들을 렌더링하고자 한다면 app 안에 들어가야만 한다.
