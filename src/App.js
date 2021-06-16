a364597e0e2c13869af8604cbb7464de3c19fb36
root <root@DESKTOP-MDAUM0B.localdomain>
2021-06-16 17:50:14 +0900

    add : useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Container({ children }) {
   const style = {
@@ -10,6 +10,13 @@ function Container({ children }) {
 
 function Count() {
   const [number, setCount] = useState(0);
+
+  useEffect(()=>{
+    // DOM 업데이트 ( 렌더링 ) 후 어떤 작업을 할것인지 useEffect Hook을 이용할 수 있다.
+    // 기존 class 컴포넌트는 단일 함수로 기능을 할 수가 없었던걸 보면 매우 편리함을 알 수 있다.
+    
+    document.title = `The number is ${number}`
+  })
 
   const addCount = () => {
     setCount((prevCount) => prevCount + 1);
