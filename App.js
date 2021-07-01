ef59abd9d950e1f45ea9841bf5dcb4ad7a88c70c
Donghyeok <tjehdgur1500@naver.com>
2021-07-02 00:14:33 +0900

    add : MaterialIcons

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default function App() {
   const [isLoading, setLoading] = useState(true);
   const [temp, setData] = useState(null);
   const [condition, setCondition] = useState(null);
+  
   useEffect(()=>{
 
     getWeather = async (latitude, longitude)=>{
--- Weather.js
+++ Weather.js
@@ -1,9 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, View, Text } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Weather({temp, condition}){
-    return <View style={styles.container}><Text>{temp}{condition}</Text></View>
+    return (
+        <View style={styles.container}>
+            <View style={styles.halfContainer}>
+            <MaterialCommunityIcons name="weather-lightning-rainy" size={100} color="black" />
+            <Text style={styles.temp}>{temp}℃</Text>
+            </View>
+            <View style={styles.halfContainer}></View>
+        </View>
+        )
 }
 
 Weather.propTypes = {
@@ -16,5 +25,14 @@ const styles = StyleSheet.create({
         flex : 1,
         alignItems : "center",
         justifyContent : "center"
+    },
+    temp : {
+       fontSize : 42,
+       textAlign:"center", 
+    },
+    halfContainer : {
+        flex: 1,
+        justifyContent : "center",
+        alignContent: "center"
     }
 })
