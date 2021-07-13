5461f7e47e9c70ace39c9b7d078f6751ca8150d7
donghyeok <tjehdgur1500@naver.com>
2021-07-13 22:07:37 +0900

    add : Vanilla Redux To Do list

--- src/index.js
+++ src/index.js
@@ -7,13 +7,34 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// action creator
+// 함수 단위로 쪼개 각자의 역할만 할 수 있게 함
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+    id: Date.now(),
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
+// reducer
+// state를 바꿀 수 있는 유일한 방법
+// 기존 state를 mutate 하지 않고 , 새 object를 만들어서 state를 리턴
 const reducer = (state = [], action) => {
-  console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      const newTodoObj = { text: action.text, id: action.id };
+      return [newTodoObj, ...state];
     case DELETE_TODO:
-      return [];
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
     default:
       return state;
   }
@@ -21,11 +42,44 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+// subscribe
+// 구독을 통해서 변경된 state를 등록한다.
+store.subscribe(() => console.log(store.getState()));
+
+// dispacth
+// action을 보내기 위한 메서드
+// action에 action creator의 객체가 전달 된다.
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (event) => {
+  const id = parseInt(event.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((todo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    li.id = todo.id;
+    li.innerText = todo.text;
+    btn.innerText = "delete";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
