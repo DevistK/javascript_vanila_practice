acf03f3ee577fe99ecd134667cc0af72b23f2fed
donghyeok <tjehdgur1500@naver.com>
2021-07-28 21:50:34 +0900

    add : tweet delete

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,12 +1,20 @@
+import { dbService } from "myBase";
 import React from "react";
 
 function Nweet({ nweetObj, isOnwer }) {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("이 트윗을 삭제 하시겠습니까 ?");
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    } else {
+    }
+  };
   return (
     <div>
       <h4>{nweetObj.text}</h4>
       {isOnwer && (
         <>
-          <button>Delete Nweet</button>
+          <button onClick={onDeleteClick}>Delete Nweet</button>
           <button>Edit Nweet</button>
         </>
       )}
